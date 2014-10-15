2ab94c000ba74aeb5de2bbc4a753290146be2ba0
Timothy L. Sanders <tsanders@hiddenjester.com>
2014-10-15 10:25:06 -0700

    I wrote a CGSizeInset func that I ended up not using but I wanted to keep it somewhere. So I added Utilities.swift to HJSKit for miscellaneous bits.

---
+++ HJSKit/Utilities.swift
@@ -0,0 +1,22 @@
+//
+//  Utilities.swift
+//  HJSKit
+//
+//  Created by Timothy Sanders on 10/15/14.
+//  Copyright (c) 2014 HiddenJester Software. All rights reserved.
+//
+
+import Foundation
+
+/*!
+	CGSize implementation of CGRectInset-like logic
+
+	:param: size The original size to inset.
+	:param: dx The amount to shrink the width. Negative values grow the width.
+	:param: dy The amount to shrink the height. Negative values grow the height.
+	
+	:returns: An inset CGSize
+*/
+public func CGSizeInset(size: CGSize, dx: CGFloat, dy: CGFloat) -> CGSize {
+	return CGSizeMake(size.width - dx, size.height - dy)
+}
