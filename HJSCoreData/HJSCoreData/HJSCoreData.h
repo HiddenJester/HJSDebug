74e6fafedae472e70bbe5b9049120861a30f751c
Timothy L. Sanders <tsanders@hiddenjester.com>
2015-04-05 00:24:10 -0700

    Fixed the big HJSKit refactor.

---
+++ HJSCoreData/HJSCoreData/HJSCoreData.h
@@ -0,0 +1,20 @@
+//
+//  HJSCoreData.h
+//  HJSCoreData
+//
+//  Created by Timothy Sanders on 2015-04-04.
+//  Copyright (c) 2015 HIddenJester Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HJSCoreData.
+FOUNDATION_EXPORT double HJSCoreDataVersionNumber;
+
+//! Project version string for HJSCoreData.
+FOUNDATION_EXPORT const unsigned char HJSCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HJSCoreData/PublicHeader.h>
+
+#import "HJSCoreDataCenter.h"
+
---
+++ HJSKeyboardHelpers/HJSKeyboardHelpers/HJSKeyboardHelpers.h
@@ -0,0 +1,19 @@
+//
+//  HJSKeyboardHelpers.h
+//  HJSKeyboardHelpers
+//
+//  Created by Timothy Sanders on 2015-04-04.
+//  Copyright (c) 2015 HIddenJester Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HJSKeyboardHelpers.
+FOUNDATION_EXPORT double HJSKeyboardHelpersVersionNumber;
+
+//! Project version string for HJSKeyboardHelpers.
+FOUNDATION_EXPORT const unsigned char HJSKeyboardHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HJSKeyboardHelpers/PublicHeader.h>
+
+
