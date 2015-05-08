cf1425078e7ef8594d6867530dcf57922de876b3
Timothy L. Sanders <tsanders@hiddenjester.com>
2015-05-08 12:09:41 -0700

    Disabled the branch appending to the version string.

--- setbuildversion.sh
+++ setbuildversion.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Copied from http://blog.jaredsinclair.com/post/97193356620/the-best-of-all-possible-xcode-automated-build
 
+# TLS 2015-05-08 Because of the requirement to make the WatchKit version info to the app versions and the ability
+# to run the script against the WatchKit app I have to set the WatchKit's info in a git hook and thereforce I can't
+# do the test for the Debug scheme. For now I've just disabled the ability to set the branch information in the
+# in the bundle version. I attempted to push it in into CFBundleShortVersionString but WatchKit insists that those
+# match as well.
 #
 # Set the build number to the current git commit count.
 # If we're using the Dev scheme, then we'll suffix the build
@@ -10,10 +15,10 @@
 #
 git=`sh /etc/profile; which git`
 appBuild=`"$git" rev-list --all |wc -l`
-if [ $CONFIGURATION = "Debug" ]; then
-branchName=`"$git" rev-parse --abbrev-ref HEAD`
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $appBuild-$branchName" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
-else
+#if [ $CONFIGURATION = "Debug" ]; then
+#branchName=`"$git" rev-parse --abbrev-ref HEAD`
+#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $appBuild-$branchName" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+#else
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $appBuild" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
-fi
+#fi
 echo "Updated ${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
